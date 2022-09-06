c16519916d1a4dcb9cf747235d1e1ad036c9c8ac
haemin <goals1005@naver.com>
2022-09-06 18:14:09 +0900

    환경 재설정

--- src/main/java/data/member/MemberDTO.java
+++ src/main/java/data/member/MemberDTO.java
@@ -2,11 +2,14 @@ package data.member;
 
 import java.sql.Timestamp;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.apache.ibatis.type.Alias;
 
-@Alias("mdto")
+@Alias("mdto") // 별명 (MemberSQL.xml에서 사용할 수 있도록)
+@Getter @Setter
 public class MemberDTO {
-	private int num;
+	private int num; //pk
 	private String name;
 	private String id;
 	private String pass;
@@ -20,93 +23,7 @@ public class MemberDTO {
 	private int auth_status;
 	private String authkey;
 	private String oauth;
-	
-	
-	public String getOauth() {
-		return oauth;
-	}
-	public void setOauth(String oauth) {
-		this.oauth = oauth;
-	}
-	public int getAuth_status() {
-		return auth_status;
-	}
-	public void setAuth_status(int auth_status) {
-		this.auth_status = auth_status;
-	}
-	public String getAuthkey() {
-		return authkey;
-	}
-	public void setAuthkey(String authkey) {
-		this.authkey = authkey;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getUrl() {
-		return url;
-	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	public String getIntroduce() {
-		return introduce;
-	}
-	public void setIntroduce(String introduce) {
-		this.introduce = introduce;
-	}
-	public String getHp() {
-		return hp;
-	}
-	public void setHp(String hp) {
-		this.hp = hp;
-	}
-	public String getPrivacy() {
-		return privacy;
-	}
-	public void setPrivacy(String privacy) {
-		this.privacy = privacy;
-	}
-	public int getNum() {
-		return num;
-	}
-	public void setNum(int num) {
-		this.num = num;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getPass() {
-		return pass;
-	}
-	public void setPass(String pass) {
-		this.pass = pass;
-	}
-	public String getPhoto() {
-		return photo;
-	}
-	public void setPhoto(String photo) {
-		this.photo = photo;
-	}
-	public Timestamp getJoin_date() {
-		return join_date;
-	}
-	public void setJoin_date(Timestamp join_date) {
-		this.join_date = join_date;
-	}
-	
-	
+
+
 	
 }
